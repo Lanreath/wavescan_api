431ce99ceca17e0cdf58f2126ceb4f709a70bf6c
Lanreath <tyfei00@gmail.com>
2023-03-10 02:43:52 +0800

    Use env variable

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
